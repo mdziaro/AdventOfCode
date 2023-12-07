c46312b72e9a63b832e50ff1ffd1e9b0accd89f3
mdziaro <mdziaro@student.agh.edu.pl>
2023-12-07 20:58:09 +0100

    Day 5_1

--- day5/day5_1.py
+++ day5/day5_1.py
@@ -1,11 +1,4 @@
 plik = open("input5.txt", "r")
-map1 = open("seed-to-soil.txt", "r")
-map2 = open("soil-to-fertilizer.txt", "r")
-map3 = open("fertilizer-to-water.txt", "r")
-map4 = open("water-to-light.txt", "r")
-map5 = open("light-to-temperature.txt", "r")
-map6 = open("temperature-to-humidity.txt", "r")
-map7 = open("humidity-to-location.txt", "r")
 
 def odczytaj(plik):
     dane = plik.readlines()
@@ -15,22 +8,28 @@ def odczytaj(plik):
         dane[i] = dane[i].split()
     return dane
 
+
 dane = odczytaj(plik)
-mapy = [map1, map2, map3, map4, map5, map6, map7]
-for i in range(len(mapy)):
-    mapy[i] = odczytaj(mapy[i])
-
 seeds = list(map(int, dane[0][1:]))
-print("maxxxxx:",max(seeds))
-for j in range(len(mapy)):
-    print(f"Processing map {j + 1}:")
-    for i in range(len(seeds)):
-        for k in range(len(mapy[j])):
-            if (seeds[i] >= int(mapy[j][k][1])) and (seeds[i] < (int(mapy[j][k][1]) + int(mapy[j][k][2]))):
-                seeds[i] = (seeds[i] - int(mapy[j][k][1])) + int(mapy[j][k][0])
-                print(f"Seed {seeds[i]} after map {j + 1} transformation.")
-                break
-        
+#print(dane)
+#print(seeds)
 
-print("Final transformed seeds:", sorted(seeds))
-print("Min location number:", min(seeds))
+mapy = []
+
+for i in range(1, len(dane)):
+    if dane[i] == []:
+        mapy.append([])
+    elif dane[i-1] == []:
+        pass
+    else:
+        mapy[-1].append(list(map(int,dane[i])))
+
+for i in range(len(mapy)):
+    for j in range(len(seeds)):
+        #print(mapy[i])
+        for k in range(len(mapy[i])):
+            dst, src, rng = mapy[i][k][0],mapy[i][k][1],mapy[i][k][2]
+            if seeds[j] >= src and seeds[j] < src+rng:
+                seeds[j] = seeds[j] - (src - dst)
+                break
+print(sorted(seeds))
