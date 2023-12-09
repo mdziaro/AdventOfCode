b1dc00133047a567635d94be2c4db5c4cc1483ef
mdziaro <mdziaro@student.agh.edu.pl>
2023-12-09 23:11:50 +0100

    Day9

---
+++ day9/day9_1.py
@@ -0,0 +1,34 @@
+plik = open("input9.txt")
+from time import sleep
+def odczytaj(plik):
+    dane = plik.readlines()
+    for i in range(len(dane)):
+        if dane[i][-1] == "\n":
+            dane[i] = dane[i][:-1]
+        dane[i] = dane[i].split()
+    return dane
+
+def extrapolate(data):
+    sub_values = ["0"]*(len(data)-1)
+    for j in range(len(data)-1):
+        sub_values[j] = data[j+1]-data[j]
+    return sub_values
+
+dane = odczytaj(plik)
+
+wynik = 0
+
+for i in range(len(dane)):
+    values = [dane[i]]
+    values[-1] = list(map(int,values[-1]))
+    while True:
+        values.append(extrapolate(values[-1]))
+        if set(values[-1]) == set([0]):
+            break
+    for j in range(len(values)-2, -1, -1):
+        values[j].append(int(values[j+1][-1])+int(values[j][-1]))
+    print(values[0][-1])
+    print(values)
+    wynik += int(values[0][-1])
+print(wynik)
+    
